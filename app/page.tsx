cad8ccd9b5b8bf84ae21aafed654275c715296ed
Uyiosa2607 <aghahowaosamudiamen03@gmail.com>
2025-02-08 13:46:41 +0100

    improved layout and redesigned  the home listing card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/header";
-import Image from "next/image";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Phone } from "lucide-react";
 import HomeListingCard from "@/components/home_listing_card";
+import Footer from "@/components/footer";
 
 const mockData = [
   {
@@ -12,13 +12,13 @@ const mockData = [
   },
   {
     name: "new apartment 2",
-    bathrooms: 2,
-    bedrooms: 2,
+    bathrooms: 1,
+    bedrooms: 1,
     price: 2000,
   },
   {
     name: "new apartment 3",
-    bathrooms: 2,
+    bathrooms: 1,
     bedrooms: 2,
     price: 2000,
   },
@@ -44,24 +44,16 @@ const mockData = [
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-[#fff]">
       <Header />
-      {/* header container */}
-      <div className="w-full mb-20">
-        <div className="w-full relative px-4 h-full">
-          {/* header banner image */}
-          <Image
-            src={"/landscape.jpg"}
-            width={1000}
-            height={10000}
-            alt="house_banner"
-            quality={100}
-            className="h-[70vh] rounded-3xl object-cover w-full "
-          />
 
-          {/* Header text and button */}
+      {/* Hero Section */}
+      <div className="w-full mb-10">
+        <div className="w-full h-[300px] relative px-4">
+          <div className="insert-0 w-full h-full bg-cover bg-center absolute bg-[url('/banner_2.jpg')] opacity-90" />
 
-          <div className="absolute text-white top-[40%] w-full">
+          {/* Hero Text and button */}
+          <div className="z-[50] relative text-white top-[40%] w-full">
             <h2 className="text-5xl mb-4 capitalize text-center font-semibold font-[Montserrat]">
               Find your dream house
             </h2>
@@ -75,25 +67,108 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* Hero section ends here */}
 
-      {/* listing section */}
-
+      {/* Details section */}
       <div className="container mx-auto w-[70%]">
-        <div className="flex items-center w-full justify-between">
+        {/* listing section title bar */}
+        <div className="flex items-center w-full mb-2.5 justify-between">
           <h3 className="font-medium text-2xl">Browse Listings</h3>
-          <div className="flex items-center gap-1">
+          <div className="flex items-center gap-1 pr-2">
             <p className="font-medium text-xs">view all</p>
             <ArrowRight size={14} />
           </div>
         </div>
 
         {/* listing container */}
-        <div className="grid grid-cols-3 space-x-2 space-y-2 px-2 items-center">
+        <div className="grid grid-cols-3 gap-3">
           {mockData.map((listing) => (
             <HomeListingCard listing={listing} key={listing.name} />
           ))}
         </div>
+        {/* listing container ends here */}
+
+        {/* why choose  us section */}
+        <div className="bg-[url('/banner.jpg')] bg-center rounded-3xl text-white bg-cover h-[300px] mt-10 w-full">
+          <div className="w-[85%] mx-auto pb-10 pt-5">
+            <h4 className="font-medium text-sm mb-2 text-yellow-400">
+              features
+            </h4>
+            <h2 className="font-semibold mb-4 text-3xl ">Why choose us</h2>
+            <p className="text-sm font-medium mb-4">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+              corrupti delectus neque nihil repudiandae similique!
+            </p>
+            <div className="flex gap-3 flex-col">
+              <div className="flex gap-2 items-center flex-row">
+                <div className="w-4 h-4 border-2 border-yellow-400 rounded-full"></div>
+                <p>Trusted Company</p>
+              </div>
+              <div className="flex  gap-2 items-center flex-row">
+                <div className="w-4 border-2 border-yellow-400 h-4 rounded-full"></div>
+                <p>Transparent Pricing</p>
+              </div>
+              <div className="flex gap-2 items-center flex-row">
+                <div className="w-4 border-2 border-yellow-400 h-4 rounded-full"></div>
+                <p>Professional Support</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* why choose us ends here */}
+
+        {/* Contact us section */}
+        <div className="mt-10 w-full  bg-[url('/contact-bg.jpg')] bg-cover rounded-3xl">
+          <div className="w-[85%] mx-auto pt-6">
+            <div className="flex gap-4 flex-row">
+              <div className="flex-1 text-white">
+                <h3>Get in touch</h3>
+                <p>Office:</p>
+                <p>office address</p>
+                <p>Work hours:</p>
+                <p>office work hour</p>
+                <p>Phone</p>
+                <div className="flex items-center flex-row gap-1">
+                  <Phone className="text-yellow-400" size={14} />
+                  <p>Phone</p>
+                </div>
+              </div>
+
+              {/* Contact Form */}
+              <form className="flex-1 flex flex-col gap-2.5 pb-6">
+                <input
+                  className="bg-transparent font-[Montserrat] text-white text-sm border-2 border-white rounded-2xl p-[6px] px-3"
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                />
+                <input
+                  className="bg-transparent font-[Montserrat] text-white text-sm border-2 border-white rounded-2xl p-[6px]"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                />
+                <input
+                  className="bg-transparent text-white font-[Montserrat] text-sm border-2 border-white rounded-2xl p-[6px]"
+                  name="phone"
+                  type="phone"
+                  placeholder="Phone"
+                />
+                <textarea
+                  className="bg-transparent  text-white font-[Montserrat] text-sm h-[140px] border-2 border-white rounded-2xl p-[6px]"
+                  name="message"
+                  placeholder="Message"
+                />
+                <button className="w-full capitalize text-center text-sm font-[Montserrat] text-neutral-800 py-2.5 rounded-3xl bg-yellow-400 font-medium">
+                  send Message
+                </button>
+              </form>
+              {/* contact form ends here */}
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
--- components/home_listing_card.tsx
+++ components/home_listing_card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Bath, BedDouble, ChevronRight } from "lucide-react";
 
 interface CardProp {
   listing: {
@@ -11,16 +12,50 @@ interface CardProp {
 
 export default function HomeListingCard({ listing }: CardProp) {
   return (
-    <div>
+    <div className="w-full">
       <Image
         alt={listing?.name}
         width={500}
         quality={100}
         height={500}
         src={"/banner.jpg"}
-        className="object-fit w-full h-[160px]"
+        className="object-fit rounded-t-3xl w-full h-[190px]"
       />
-      <p>{listing?.name}</p>
+      <div className="p-[8px] rounded-b-3xl bg-stone-100">
+        <p className="text-base mb-2.5 font-semibold font-[Montserrat]">
+          {listing?.name}
+        </p>
+        <div>
+          <div className="flex items-center mb-2 gap-1.5 flex-row">
+            <BedDouble size={14} />
+            {listing?.bedrooms === 1 ? (
+              <p className="text-sm font-medium">{`${listing?.bedrooms} Bedroom`}</p>
+            ) : (
+              <p className="text-sm font-medium">{`${listing?.bedrooms} Bedrooms`}</p>
+            )}
+          </div>
+          <div className="flex items-center mb-2 gap-1.5 flex-row">
+            <Bath size={14} />
+            {listing?.bathrooms === 1 ? (
+              <p className="text-sm font-medium">{`${listing?.bathrooms} Bathroom`}</p>
+            ) : (
+              <p className="text-sm font-medium">{`${listing?.bathrooms} Bathrooms`}</p>
+            )}
+          </div>
+          <div className="w-[95%] margin-auto my-1.5 border-b-2 border-neutral-800" />
+          <div className="flex items-center w-full justify-between flex-row">
+            <p className="text-sm font-semibold text-neutral-800">
+              ${listing?.price}
+            </p>
+            <div className="flex items-center ">
+              <p className="text-sm font-medium text-neutral-800">
+                view details
+              </p>
+              <ChevronRight size={13} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
